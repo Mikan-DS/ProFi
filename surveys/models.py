2e3327143c0cc66893248d4f0be419df82ac422c
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-12-04 15:41:47 +0300

    database

---
+++ surveys/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+class Survey(models.Model):
+    title = models.CharField(max_length=70)
+
+
+class SurveyScoreVariable(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    result_title = models.CharField(max_length=70)
+    description = models.TextField()
+
+
+class SurveyQuestionPairTypeStatement(models.Model):
+    statement = models.CharField(max_length=256)
+    scoreVariable = models.ForeignKey(SurveyScoreVariable, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class SurveyQuestionPairType(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+    statementA = models.ForeignKey(SurveyQuestionPairTypeStatement, on_delete=models.CASCADE, related_name='statementsA')
+    statementB = models.ForeignKey(SurveyQuestionPairTypeStatement, on_delete=models.CASCADE, related_name='statementsB')
+
+
+class EducationalInstitution(models.Model):
+    name = models.CharField(max_length=256)
+
+class Profession(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+class Specialty(models.Model):
+    institution = models.ForeignKey(EducationalInstitution, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    link = models.URLField(max_length=256, blank=True, null=True)
+
+class ProfessionSpecialty(models.Model):
+    profession = models.ForeignKey(Profession, on_delete=models.CASCADE)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+
+class TestResultProfession(models.Model):
+    profession = models.ForeignKey(Profession, on_delete=models.CASCADE)
+    survey_score_variable = models.ForeignKey(
+        SurveyScoreVariable,
+        on_delete=models.CASCADE,
+        related_name='professionSurveyScoreVariable'
+    )
+
+
+# class SurveyQuestionTrinary(models.Model):
+#     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+#
+#     question = models.CharField(max_length=256)
+#
+#     yesScoreVariable = models.ForeignKey(SurveyScoreVariable, on_delete=models.CASCADE, null=True, blank=True)
+#     noScoreVariable = models.ForeignKey(SurveyScoreVariable, on_delete=models.CASCADE, null=True, blank=True)
+#     noneScoreVariable = models.ForeignKey(SurveyScoreVariable, on_delete=models.CASCADE, null=True, blank=True)
+#
+
+
+
+
